2a9edcc6ecc8d525ec614b5ac66e503607bdab50
orgavriel <orgavriel10@gmail.com>
2018-06-08 13:15:44 +0300

    gavriel faivlis

--- MazeSolver_ex2/src/com/start/LineFollwer.java
+++ MazeSolver_ex2/src/com/start/LineFollwer.java
@@ -1,36 +1,67 @@
 package com.start;
 
 import com.logger.Logger;
+import com.robot.Motors;
 import com.robot.Sensors;
 import com.robot.Utils;
 
-import controllers.BaseController;
-import controllers.controllers;
+import controllers.PIDController;
+
 import lejos.nxt.Button;
 import lejos.nxt.ButtonListener;
 import lejos.nxt.LCD;
+import lejos.nxt.Sound;
+import lejos.util.Delay;
 
 public class LineFollwer 
 {
 
-	public static BaseController contruller = null;
-	public static controllers _factoryControlls = new controllers();
+	public static PIDController contruller = null;
+//	public static controllers _factoryControlls = new controllers();
 	public static int s_low = 0;
 	public static int s_high = 0;
 	
+	
+	
 	public static void main(String[] args) 
 	{
+		Motors motors = new Motors();
+		int powerAdd = 40;
+		int wallCount = 0;
+		
 		try
 		{		
 			//get low and high
 			getHighAndLow();
 			
-			contruller = _factoryControlls.getShape("pid", s_low, s_high);
+			contruller = new PIDController(s_low, s_high);
 		//	contruller = _factoryControlls.getShape("sensor_test", s_low, s_high);
 			
 			//while loop		
 			while (!Button.ESCAPE.isDown() && !Sensors.isExit())
-				contruller.run();
+			{				
+				if(Sensors.isWall())
+				{
+					wallCount++;
+					contruller.restart();	
+					
+					motors.setPower(-40, -40);
+					Delay.msDelay(400);									
+					
+					motors.setPower(powerAdd + wallCount*10, -powerAdd);
+					Delay.msDelay(250);
+					
+					motors.setPower(powerAdd, powerAdd);
+					Delay.msDelay(350);
+					
+					motors.setPower(0, 0);					
+				}
+				else	
+				{
+					wallCount = 0;
+					contruller.run();
+				}
+			}
 			
 			//finish the controller task
 			contruller.finish();
